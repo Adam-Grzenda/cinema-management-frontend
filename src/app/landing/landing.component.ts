d5c3d987d619d386cf56d4677aed2289189904b6
adamg <adam.grzenda@gmail.com>
2021-12-26 19:08:56 +0100

    fix scaling issues

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-landing',
@@ -7,7 +7,7 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
-  sidebarOpen: boolean = true;
+  sidebarOpen: boolean = false;
 
   constructor() { }
 
