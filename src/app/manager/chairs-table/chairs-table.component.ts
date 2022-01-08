1b1d1f0289dd7a89f696bbfa7ff42ad5018aa667
matfrac <mateusz.p.frackowiak@gmail.com>
2022-01-08 17:00:53 +0100

    didsplaying issues - probably working

--- src/app/manager/chairs-table/chairs-table.component.ts
+++ src/app/manager/chairs-table/chairs-table.component.ts
@@ -5,29 +5,54 @@ import {Sort} from "@angular/material/sort";
 import {ChairService} from "../../services/chair.service";
 import {MatTableDataSource} from "@angular/material/table";
 
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
+
+
+
 @Component({
   selector: 'app-chairs-table',
   templateUrl: './chairs-table.component.html',
   styleUrls: ['./chairs-table.component.css']
 })
 export class ChairsTableComponent implements OnInit {
-  //@ViewChild(MdbTableDirective, {static: true}) mdbTable:MdbTableDirective;
+
+  //#TODO displaying issues
 
   @Input()
   hall_id: number;
 
   chairs: Chair[];
 
-  sortedChairs: Chair[];
+  sortedChairs: Chair[] = [];
 
   searchText: string = '';
-  previous: string;
 
-  dataSource: MatTableDataSource<Chair>;
+  dataSource = new MatTableDataSource(this.sortedChairs)
+  displayedColumns: ['id', 'hallRow', 'hallColumn', 'chairType'];
+
+  dataSource2 = new MatTableDataSource(ELEMENT_DATA);
 
   constructor(
     private cinemaHallService: CinemaHallService,
-    private chairService:ChairService
+    private chairService: ChairService
   ) {
   }
 
@@ -69,10 +94,6 @@ export class ChairsTableComponent implements OnInit {
     })
   }
 
-  search() {
-    //const prev = this.
-  }
-
   delete(chair: Chair) {
     this.chairService.delete(chair).subscribe(c => {
       console.log("deleted: " + chair.id);
@@ -83,6 +104,7 @@ export class ChairsTableComponent implements OnInit {
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+    this.dataSource2.filter = filterValue.trim().toLowerCase();
   }
 }
 
