2d692a55bcf386206f91602b2ef71b54d8b7abe8
adamg <adam.grzenda@gmail.com>
2022-01-15 00:37:26 +0100

    add cinema number mapping

--- src/model/user/keycloak-user.ts
+++ src/model/user/keycloak-user.ts
@@ -10,5 +10,6 @@ export class UserInfo {
     "cinema-management-frontend": {
       roles: string[]
     }
-  }
+  };
+  cinema: number;
 }
--- src/model/user/user.ts
+++ src/model/user/user.ts
@@ -6,6 +6,7 @@ export class User {
   name: string;
   surname: string;
   roles: string[];
+  cinema: number;
 
   static fromKeycloakUserInfo(keycloakUserInfo: any): User {
     let userInfo: KeycloakUser = new KeycloakUser();
@@ -15,6 +16,7 @@ export class User {
     user.name = userInfo.info.given_name;
     user.surname = userInfo.info.family_name;
     user.roles = userInfo.info.resource_access["cinema-management-frontend"].roles;
+    user.cinema = userInfo.info.cinema;
     return user;
   }
 
